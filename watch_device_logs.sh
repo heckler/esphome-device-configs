8404cf58b64d57b69b4ccf53dd44b1274e419e49
Claudio <claudio.andre@heckler.com.br>
2022-10-12 17:35:57 -0300

    major refactoring leveraging substitutions and packages to reduce duplication

--- watch_device_logs.sh
+++ watch_device_logs.sh
@@ -5,19 +5,20 @@
 
 echo "Starting to watch ESPHome logs ..."
 
-rm *.log
+mkdir -p logs
+rm logs/*.log
 
-esphome balcony_sensors.yaml logs    2>&1 >> log_balcony_sensors.log    &
-esphome balcony_sensors_02.yaml logs 2>&1 >> log_balcony_sensors_02.log &
-esphome fireplace_light.yaml logs    2>&1 >> log_fireplace_light.log    &
-esphome freezer_sensors.yaml logs    2>&1 >> log_freezer_sensors.log    &
-esphome irrigation_pump.yaml logs    2>&1 >> log_irrigation_pump.log    &
-esphome wakeup_light_01r.yaml logs   2>&1 >> log_wakeup_light_01r.log   &
-esphome wakeup_light_02g.yaml logs   2>&1 >> log_wakeup_light_02g.log   &
+esphome balcony_sensors.yaml logs    2>&1 >> logs/log_balcony_sensors.log    &
+esphome balcony_sensors_02.yaml logs 2>&1 >> logs/log_balcony_sensors_02.log &
+esphome fireplace_light.yaml logs    2>&1 >> logs/log_fireplace_light.log    &
+esphome freezer_sensors.yaml logs    2>&1 >> logs/log_freezer_sensors.log    &
+esphome irrigation_pump.yaml logs    2>&1 >> logs/log_irrigation_pump.log    &
+esphome wakeup_light_01r.yaml logs   2>&1 >> logs/log_wakeup_light_01r.log   &
+esphome wakeup_light_02g.yaml logs   2>&1 >> logs/log_wakeup_light_02g.log   &
 
 echo "Watching ESPHome logs in the background:"
 
-ls -la log_*.log
+ls -la logs/log_*.log
 
 echo "To kill them:  ps | grep esphome"
 
